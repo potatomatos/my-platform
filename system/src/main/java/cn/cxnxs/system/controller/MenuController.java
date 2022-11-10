767d5482ba99e0ebf99ec3b62143dbc4c092f932
potatomatos <782399171@qq.com>
2022-11-10 18:21:45 +0800

    菜单接口

--- system/src/main/java/cn/cxnxs/system/controller/MenuController.java
+++ system/src/main/java/cn/cxnxs/system/controller/MenuController.java
@@ -3,11 +3,10 @@ package cn.cxnxs.system.controller;
 
 import cn.cxnxs.common.core.entity.TreeVo;
 import cn.cxnxs.common.web.annotation.ResponseResult;
+import cn.cxnxs.system.entity.SysMenu;
 import cn.cxnxs.system.service.IMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,13 +23,31 @@ public class MenuController {
 
     @ResponseResult
     @GetMapping("userMenus")
-    public List<TreeVo> getUserMenusTree(){
+    public List<TreeVo> getUserMenusTree() {
         return menuService.getUserMenusTree();
     }
 
     @ResponseResult
     @GetMapping("menuTree")
-    public List<TreeVo> getMenusTree(){
+    public List<TreeVo> getMenusTree() {
         return menuService.getMenusTree();
+    }
+
+    @ResponseResult
+    @PostMapping("add")
+    public Integer add(@RequestBody SysMenu sysMenu) {
+        return menuService.addMenu(sysMenu);
+    }
+
+    @ResponseResult
+    @PostMapping("update")
+    public Integer update(@RequestBody SysMenu sysMenu) {
+        return menuService.updateMenu(sysMenu);
+    }
+
+    @ResponseResult
+    @PostMapping("delete/{id}")
+    public Integer deleteMenu(@PathVariable("id") Integer id) {
+        return menuService.deleteMenu(id);
     }
 }
