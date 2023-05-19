22a394acccd1d7b8406e4beb83631a6e6080163f
potatomatos <782399171@qq.com>
2023-05-19 16:44:13 +0800

    修改打包方式

--- system/src/main/java/cn/cxnxs/system/controller/SystemController.java
+++ system/src/main/java/cn/cxnxs/system/controller/SystemController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
